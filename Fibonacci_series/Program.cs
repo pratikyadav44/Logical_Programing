65b4b469a32fe19730d9e233da362ed6bb14e0b2
pratikyadav44 <pratik.yadav2@gmail.com>
2023-03-01 16:55:19 +0530

    [

---
+++ Fibonacci_series/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Fibonacci_series
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 1;
+            int c;
+            for (int i = 0; i < 10; i++) 
+            {
+                c = a + b;
+                Console.WriteLine(c);
+                a = b; 
+                b = c;
+            }
+            Console.ReadLine();
+        }
+    }
+}
